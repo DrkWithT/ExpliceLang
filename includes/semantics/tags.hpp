77036d491f8f0fe2f0715ee6d524b5049bd7d6f8
drkwitht <derektan3510@gmail.com>
2025-05-16 21:56:00 -0700

    Added semantic tags e.g types.

---
+++ includes/semantics/tags.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+// #include <vector>
+#include <string>
+
+namespace XLang::Semantics {
+    enum class OpTag : unsigned char {
+        none,
+        access,
+        negate,
+        multiply,
+        divide,
+        add,
+        subtract,
+        cmp_equ,
+        cmp_neq,
+        cmp_lt,
+        cmp_gt,
+        logic_and,
+        logic_or
+    };
+
+    enum class ValuingTag : unsigned char {
+        x_in_value, // anything mutable
+        x_out_value // primitive, constant, or read-only
+    };
+
+    enum class TypeTag : unsigned char {
+        x_type_bool,
+        x_type_int,
+        x_type_float,
+        x_type_unknown
+    };
+
+    [[nodiscard]] std::string create_type_name(TypeTag single_tag);
+
+    // [[nodiscard]] std::string create_type_name(const std::vector<TypeTag>& tupled_tags);
+
+    // [[nodiscard]] std::string create_type_name(TypeTag item_tag, int count);
+}
---
+++ src/semantics/tags.cpp
@@ -0,0 +1,16 @@
+#include <array>
+#include <string_view>
+#include "semantics/tags.hpp"
+
+namespace XLang::Semantics {
+    std::string create_type_name(TypeTag single_tag) {
+        static constinit std::array<std::string_view, static_cast<std::size_t>(TypeTag::x_type_unknown) + 1> dirty_names = {
+            "bool",
+            "int",
+            "float",
+            "unknown"
+        }; // quick and dirty mapping from type tags to names for comparisons...
+
+        return dirty_names[static_cast<int>(single_tag)].data();
+    }
+}
