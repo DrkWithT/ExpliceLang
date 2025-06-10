b543a167d6adc6648fda147a7a9c9c5e0368184b
drkwitht <derektan3510@gmail.com>
2025-06-10 14:10:49 -0700

    Added semantic checker test.

--- src/xplice.cpp
+++ src/xplice.cpp
@@ -3,9 +3,9 @@
 #include <print>
 #include "frontend/files.hpp"
 #include "frontend/parser.hpp"
+#include "semantics/analysis.hpp"
 #include "codegen/graph_pass.hpp"
 #include "codegen/emit_pass.hpp"
-// #include "codegen/disassembler.hpp"
 #include "vm/vm.hpp"
 
 using namespace XLang;
@@ -19,11 +19,26 @@ using namespace XLang;
     auto [ast, parse_errors] = parser();
 
     if (!parse_errors.empty()) {
+        std::print("Parse errors of file at {}:\n\n", path_cstr);
+
         for (const auto& [msg, culprit] : parse_errors) {
-            std::print(std::cerr, "Syntax Error:\nCulprit '{}' at [{}:{}]\nNote: {}\n\n", Frontend::peek_lexeme(culprit, source_sv), culprit.line, culprit.column, msg);
+            std::print(std::cerr, "Culprit '{}' at [{}:{}]\nNote: {}\n\n", Frontend::peek_lexeme(culprit, source_sv), culprit.line, culprit.column, msg);
         }
 
-        throw std::logic_error {"Compilation failed: parse errors found."};
+        throw std::logic_error {"Compilation failed: parse error(s) found."};
+    }
+
+    Semantics::SemanticsPass sema {source_sv};
+    auto sema_errors = sema(ast);
+
+    if (!sema_errors.empty()) {
+        std::print(std::cerr, "Semantic errors of file '{}':\n", path_cstr);
+
+        for (const auto& [message, culprit_token] : sema_errors) {
+            std::print(std::cerr, "At [ln {}]\nNote: {}\n\n", culprit_token.line, message);
+        }
+
+        throw std::logic_error {"Compilation failed: semantic error(s) found."};
     }
 
     Codegen::GraphPass ir_emitter {source_sv};
@@ -63,7 +78,7 @@ int main(int argc, char* argv[]) {
             return 1;
         }
     } catch (const std::logic_error& compile_error) {
-        std::print(std::cerr, "CompileError:\n{}\n", compile_error.what());
+        std::print(std::cerr, "Compile Error:\n{}\n", compile_error.what());
         return 1;
     } catch (const std::runtime_error& vm_error) {
         std::print(std::cerr, "RuntimeError:\n{}\n", vm_error.what());
---
+++ tests/xlang_test_sema.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <print>
+#include <string_view>
+#include <string>
+#include "frontend/files.hpp"
+#include "frontend/parser.hpp"
+#include "semantics/analysis.hpp"
+
+using namespace XLang;
+
+[[nodiscard]] bool test_sema_on(std::string_view source_view, std::string_view file_name) {
+    Frontend::Parser parser {source_view};
+
+    auto parse_result = parser();
+
+    if (const auto& parse_errors = parse_result.errors; !parse_errors.empty()) {
+        std::print(std::cerr, "Parse errors for file '{}':\n", file_name);
+
+        for (const auto& [msg, culprit] : parse_errors) {
+            std::print(std::cerr, "Culprit '{}' at [{}:{}]\nNote: {}\n\n", Frontend::peek_lexeme(culprit, source_view), culprit.line, culprit.column, msg);
+        }
+
+        return false;
+    }
+
+    Semantics::SemanticsPass sema {source_view};
+    auto sema_errors = sema(parse_result.decls);
+
+    if (!sema_errors.empty()) {
+        std::print(std::cerr, "Semantic errors in file '{}':\n", file_name);
+
+        for (const auto& [message, culprit_token] : sema_errors) {
+            std::print(std::cerr, "At [ln {}]\nNote: {}\n\n", culprit_token.line, message);
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::print(std::cerr, "usage: ./xlang_test_sema <test-source-file>\n");
+        return 1;
+    }
+
+    std::string source = Frontend::read_file(argv[1]);
+
+    if (!test_sema_on(source, argv[1])) {
+        return 1;
+    }
+}
